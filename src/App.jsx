bb4e8d8fa36b4e8f92cda486cc4769e4f837eb4c
LukIt4InDr <lukit4.gabriel@gmail.com>
2025-04-28 19:36:46 -0300

    ~

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,19 @@ import './App.css'
 export function App () {
   return (
     <>
-      <TwitterFollowCard userName={"shirakamifubuki"} name={"白上フブキ🌽フブキングダム👑"} isFollowing></TwitterFollowCard>
+      <div className='tw-follow-card-suggest'>
+        <header className='tw-follow-card-header'>
+          <h2>Tal vez te guste</h2>
+        </header>
+
+        <TwitterFollowCard userName={"shirakamifubuki"} name={"白上フブキ🌽フブキングダム👑"} isFollowing></TwitterFollowCard>
+        <TwitterFollowCard userName={"shirakamifubuki"} name={"白上フブキ🌽フブキングダム👑"} isFollowing></TwitterFollowCard>
+        <TwitterFollowCard userName={"shirakamifubuki"} name={"白上フブキ🌽フブキングダム👑"} isFollowing></TwitterFollowCard>
+
+        <footer className='tw-follow-card-footer'>
+          <span>Mostrar más</span>
+        </footer>
+      </div>
     </>
   )
 }
