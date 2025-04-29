0c43919c4cfedfbfb5ee7da1a1d6534b64cae2e5
LukIt4InDr <lukit4.gabriel@gmail.com>
2025-04-29 16:01:07 -0300

    follow/unfollow

--- src/TwitterFollowCard.jsx
+++ src/TwitterFollowCard.jsx
@@ -1,15 +1,19 @@
+import { useState } from "react"
+
 export function TwitterFollowCard ({ userName, name, isFollowing }) {
-    const buttonClassName = isFollowing ? 'tw-follow-card-button is-following' : 'tw-follow-card-button'
-    const buttonText = isFollowing ? 'Siguiendo' : 'Seguir'
+    const [followingState, setFollowingState] = useState(isFollowing)
+
+    const buttonClassName = followingState ? 'tw-follow-card-button is-following' : 'tw-follow-card-button'
+    const buttonText = followingState ? 'Siguiendo' : 'Seguir'
     
     const Follow = () => {
-        console.log("Siguiendo")
+        setFollowingState(!followingState)
     }
 
     return (
         <article className="tw-follow-card">
             <div className="tw-follow-card-user">
-                <img src={`https://unavatar.io/${userName}`} alt="avatar iamge" className="tw-follow-card-avatar" />
+                <img src={`https://unavatar.io/x/${userName}`} alt="avatar iamge" className="tw-follow-card-avatar" />
                 <div className="tw-follow-card-info">
                     <strong className="tw-follow-card-name">{name}</strong>
                     <span className="tw-follow-card-user-name">@{userName}</span>
